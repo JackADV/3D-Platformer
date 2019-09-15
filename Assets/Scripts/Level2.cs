9d76b7dceda3d6c3a6b0413f456b8aa2577990b8
Jack <47583484+JackADV@users.noreply.github.com>
2019-09-15 15:07:32 +1000

    Update

---
+++ Assets/Scripts/Level2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2 : MonoBehaviour
+{
+    public GameObject exit;
+    public string levelToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exit.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay()
+    {
+        if (other.gameObject.tag == "player")
+        {
+            exit(true);
+
+        }
+        
+
+    }
+}
