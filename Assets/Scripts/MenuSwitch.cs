3ce287bbed4e06fccc62620753060b653c18e300
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-09-17 10:13:22 +1000

    Added main menu

---
+++ Assets/Scripts/MenuSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This script allows us to switch from the Main Menu scene, to the Level 1 scene
+
+public class MenuSwitch : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
